65e2e3e46201c82a9d99b7a8e26bb48ea06cd43c
jajmo <jajmo@stevens.edu>
2016-11-17 17:00:27 -0500

    Skip replies and add text to nlp output

--- nlp.py
+++ nlp.py
@@ -7,12 +7,15 @@ class NLP():
 
     def processTweets(self):
         for tweet in self.tweets:
+            if tweet['text'][0] == '@':
+                continue
             self.polarity[tweet['id']] = {}
             self.polarity[tweet['id']]['polarity'] = self.getPolarity(tweet)
             self.polarity[tweet['id']]['date'] = tweet['created_at']
             self.polarity[tweet['id']]['favorites'] = tweet['favorite_count']
             self.polarity[tweet['id']]['retweets'] = tweet['retweet_count']
             self.polarity[tweet['id']]['is_retweet'] = tweet['text'][0:2] == "RT"
+            self.polarity[tweet['id']]['text'] = tweet['text']
         return self.polarity
 
     def getPolarity(self, tweet):
