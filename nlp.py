08d47e13b77f9d586640c0e73079a5bd3c972dca
jajmo <jajmo@stevens.edu>
2016-12-01 16:36:31 -0500

    dater

--- nlp.py
+++ nlp.py
@@ -1,22 +1,30 @@
 from textblob import TextBlob
+import collections
 
 class NLP():
     def __init__(self, tweets):
-        self.tweets = tweets
-        self.polarity = {}
+        self.tweets = sorted(tweets, key = lambda x: x['id'])
+        self.polarity = collections.OrderedDict({})
 
     def processTweets(self):
+        prevID = None
+        prevDate = None
         for tweet in self.tweets:
-            if tweet['text'][0] == '@':
+            if tweet['text'][0] == '@' or tweet['created_at'][0:10] == prevDate:
                 continue
+
+            if (prevID != None):
+                self.polarity[prevID]['next'] = tweet['id']
             self.polarity[tweet['id']] = {
                 'polarity': self.getPolarity(tweet),
                 'date': tweet['created_at'],
-                'favorites': tweet['favorite_count'],
+                'likes': tweet['favorite_count'],
                 'retweets': tweet['retweet_count'],
-                'is_retweet': tweet['text'][0:2] == "RT",
+                'is_retweet': tweet['text'][1:2] == "RT",
                 'text': tweet['text']
             }
+            prevID = tweet['id']
+            prevDate = tweet['created_at'][0:10]
 
         return self.polarity
 
--- process_tweets.py
+++ process_tweets.py
@@ -1,15 +1,45 @@
 import nlp
 import json
+import datetime
+from stockReader import StockThing
 
 class ProcessTweets:
-
     def __init__(self, filename):
         self.filename = filename
 
     def process(self):
         with open(self.filename, "r") as tweets:
             x = nlp.NLP(json.loads(tweets.read()))
-        return json.dumps(x.processTweets())
+        #return json.dumps(x.processTweets(), indent = 4, separators = (',', ': '), sort_keys = True)
+        return x.processTweets()
 
-if __name__ == "__main__":
-    print(ProcessTweets("elonmusk_weets.json").process())
+
+tweets = ProcessTweets("elonmusk_tweets.json").process()
+timeFormat ='%a %b %d %H:%M:%S %z %Y' 
+increase = 0
+decrease = 0
+total = 0
+no = 0
+for tweetid, tweet in tweets.items():
+    if 'next' not in tweet:
+        break
+    tweetDate = datetime.datetime.strptime(tweet['date'], timeFormat).strftime('%m/%d/%Y')
+    nextTweetDate = datetime.datetime.strptime(tweets[tweet['next']]['date'], timeFormat).strftime('%m/%d/%Y')
+    change = StockThing.getStockChange(tweetDate, nextTweetDate)
+    if type(change) != str:
+        if tweet['polarity'] >= 0 and change >= 0:
+            increase += 1
+        elif tweet['polarity'] < 0 and change < 0:
+            decrease += 1
+        else:
+            no += 1
+        total += 1
+
+print('Stats:')
+print('--------------------')
+print('Increase:', increase)
+print('Decrease:', decrease)
+print('No correlation:', no)
+print('Total:', total)
+print('--------------------')
+print('Average successfully correlated:', (increase + decrease) / total)
