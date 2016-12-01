fc4a1523e5214ea102d55bdcfd1eb181f2ae462b
Bradford Smith <bsmith8@stevens.edu>
2016-12-01 15:28:26 -0500

    Update comment format
    Found spelling error in function docstring, also found useless test
    comment. Fixed these and re-wrapped docstring comment at 80 characters.

--- stockReader.py
+++ stockReader.py
@@ -9,9 +9,9 @@ class StockThing:
     """
     startDay - m/d/yyyy
     endDay - m/d/yyyy
-    return - the difference in the adjusted close price of the stock from startDay
-             to endDay as a flot, or the strings 'Invalid startDay' or 'Invalid
-             endDay' if either the start or end day could not be found
+    return - the difference in the adjusted close price of the stock from
+             startDay to endDay as a float, or the strings 'Invalid startDay' or
+             'Invalid endDay' if either the start or end day could not be found
     """
     def getStockChange(startDay, endDay):
         startPrice = None
@@ -20,7 +20,6 @@ class StockThing:
         with open('tsla_stock.csv', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for line in reader:
-                #print(line['Date'], line['Open'], line['Adj Close'])
                 if line['Date'] == startDay:
                     startPrice = line['Adj Close']
                 if line['Date'] == endDay:
