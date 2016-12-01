ca5b7f3f80083548718b9a8bcffe4bf17ec4a62a
Bradford Smith <bsmith8@stevens.edu>
2016-12-01 15:23:07 -0500

    Wrap getStockChange in class
    To make the function easier to call it has been wrapped in a class.

--- stockReader.py
+++ stockReader.py
@@ -1,35 +1,35 @@
 # Bradford Smith (bsmith8)
 # CS 521 Project stockReader.py
-# 11/17/2016
+# 12/01/2016
 
 import csv
 
-"""
-startDay - m/d/yyyy
-endDay - m/d/yyyy
-return - the difference in the adjusted close price of the stock from startDay
-         to endDay as a flot, or the strings 'Invalid startDay' or 'Invalid
-         endDay' if either the start or end day could not be found
-"""
-def getStockChange(startDay, endDay):
-    startPrice = None
-    endPrice = None
+class StockThing:
 
-    with open('tsla_stock.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for line in reader:
-            #print(line['Date'], line['Open'], line['Adj Close'])
-            if line['Date'] == startDay:
-                startPrice = line['Adj Close']
-            if line['Date'] == endDay:
-                endPrice = line['Adj Close']
-    csvfile.close()
+    """
+    startDay - m/d/yyyy
+    endDay - m/d/yyyy
+    return - the difference in the adjusted close price of the stock from startDay
+             to endDay as a flot, or the strings 'Invalid startDay' or 'Invalid
+             endDay' if either the start or end day could not be found
+    """
+    def getStockChange(startDay, endDay):
+        startPrice = None
+        endPrice = None
 
-    if (startPrice == None):
-        return 'Invalid startDay'
-    elif(endPrice == None):
-        return 'Invalid endDay'
-    else:
-        return float(endPrice) - float(startPrice)
+        with open('tsla_stock.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for line in reader:
+                #print(line['Date'], line['Open'], line['Adj Close'])
+                if line['Date'] == startDay:
+                    startPrice = line['Adj Close']
+                if line['Date'] == endDay:
+                    endPrice = line['Adj Close']
+        csvfile.close()
 
-print(getStockChange('10/16/2016', '10/17/2016'))
+        if (startPrice == None):
+            return 'Invalid startDay'
+        elif(endPrice == None):
+            return 'Invalid endDay'
+        else:
+            return float(endPrice) - float(startPrice)
